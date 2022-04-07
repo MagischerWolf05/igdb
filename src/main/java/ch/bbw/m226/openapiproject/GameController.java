f778a3b76d457a11d8fafc4476fef0e305d4531d
Remo Waelchli <remo.waelchli@icloud.com>
2022-04-07 23:15:28 +0200

    Controller und JSON Backend erweitert mit komplettem CRUD.

--- src/main/java/ch/bbw/m226/openapiproject/GameController.java
+++ src/main/java/ch/bbw/m226/openapiproject/GameController.java
@@ -2,25 +2,96 @@ package ch.bbw.m226.openapiproject;
 
 import ch.bbw.m226.openapi.generated.controller.GamesApi;
 import ch.bbw.m226.openapi.generated.dto.Game;
+import ch.bbw.m226.openapi.generated.dto.Platform;
 import ch.bbw.m226.openapi.generated.dto.PonyDto;
+import ch.bbw.m226.openapi.generated.dto.Publisher;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 @Validated
 @RestController
 public class GameController implements GamesApi {
-    private final ArrayList<Game> Games = new ArrayList<>();
+
+    private ArrayList<Game> Games = new ArrayList<>();
+    private ArrayList<Platform> Platforms = new ArrayList<>();
 
     private final Random random = new Random();
 
+    private final File gameFile = new File("Data/Games.json");
+    public GameController(){
+        try {
+            Games = new ObjectMapper().readValue(gameFile, new TypeReference<ArrayList<Game>>(){});
+
+            var platformFile = new File("Data/Platforms.json");
+            Platforms = new ObjectMapper().readValue(platformFile, new TypeReference<ArrayList<Platform>>(){});
+
+            for (int i = 0; i < Games.size(); i++) {
+                var game = Games.get(i);
+                var gamePlatforms = getPlatforms(game.getPlatformIDs());
+                game.setPlatforms(gamePlatforms);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    private ArrayList<Platform> getPlatforms(List<Integer> ids){
+        var gamePlatforms = new ArrayList<Platform>();
+        for (int j = 0; j < ids.size(); j++) {
+            var id = ids.get(j);
+            var platformFound = Platforms.stream().filter(x -> x.getId().equals(id)).toList().get(0);
+            gamePlatforms.add(platformFound);
+        }
+        return gamePlatforms;
+    }
+
+    private void save(){
+        //set PlatformIDs and delete Platforms awayy
+
+        //var allPlatforms = Games.stream().flatMap(x -> x.getPlatforms().stream()).toList();
+        //Remove
+        var gameSave = Games;
+        HashMap<Integer, List<Platform>> platforms = new HashMap<>();
+
+        for (int i = 0; i < gameSave.size(); i++) {
+            var game = gameSave.get(i);
+            platforms.put(game.getId(), game.getPlatforms());
+            game.setPlatformIDs(game.getPlatforms().stream().map(Platform::getId).toList());
+            game.setPlatforms(null);
+        }
+        try {
+            new ObjectMapper().writeValue(gameFile, gameSave);
+            for (int i = 0; i < Games.size(); i++) {
+                var game = Games.get(i);
+                game.setPlatforms(platforms.get(game.getId()));
+            }
+        }
+        catch (Exception e){
+            System.out.println("Could not save Games");
+        }
+
+
+    }
+
+
     @Override
     public ResponseEntity<Game> addGame(Game game) {
+
         game.setId(random.nextInt(Integer.MAX_VALUE));
+        //sets platform objects
+        var gamePlatforms = getPlatforms(game.getPlatformIDs());
+        game.setPlatforms(gamePlatforms);
         Games.add(game); // yolo ignoring conflicts
+        save();
         return ResponseEntity.status(HttpStatus.OK)
                 .body(game);
         //return null;
@@ -31,6 +102,7 @@ public class GameController implements GamesApi {
         for (int i = 0; i< Games.size(); i++ ) {
             if(Games.get(i).getId() == id){
                 Games.remove(i);
+                save();
                 return ResponseEntity.status(HttpStatus.OK).body(null);
             }
         }
@@ -52,6 +124,8 @@ public class GameController implements GamesApi {
                 possibleGame = game;
                 possibleGame.setId(id);
                 //save
+                possibleGame.setPlatformIDs(possibleGame.getPlatforms().stream().map(Platform::getId).toList());
+                save();
                 return ResponseEntity.status(HttpStatus.OK)
                         .body(possibleGame);
             }
--- src/main/java/ch/bbw/m226/openapiproject/PublisherController.java
+++ src/main/java/ch/bbw/m226/openapiproject/PublisherController.java
@@ -1,27 +1,57 @@
 package ch.bbw.m226.openapiproject;
 
 import ch.bbw.m226.openapi.generated.controller.PublishersApi;
+import ch.bbw.m226.openapi.generated.dto.Game;
+import ch.bbw.m226.openapi.generated.dto.Platform;
 import ch.bbw.m226.openapi.generated.dto.PonyDto;
 import ch.bbw.m226.openapi.generated.dto.Publisher;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import io.swagger.v3.core.util.Json;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 @Validated
 @RestController
 public class PublisherController implements PublishersApi {
 
+    private final File publisherFile =  new File("Data/Publishers.json");
+
     private ArrayList<Publisher> Publishers = new ArrayList<>();
 
     private final Random random = new Random();
+
+    PublisherController() {
+        try{
+            Publishers = new ObjectMapper().readValue(publisherFile, new TypeReference<ArrayList<Publisher>>(){});
+        }
+        catch (Exception e){}
+    }
+
+    private void save(){
+        try {
+            new ObjectMapper().writeValue(publisherFile, Publishers);
+        }
+        catch (Exception e){
+            System.out.println("Could not save Games");
+        }
+    }
+
 
     @Override
     public ResponseEntity<Publisher> addPublisher(Publisher publisher) {
         publisher.setId(random.nextInt(Integer.MAX_VALUE));
         Publishers.add(publisher); // yolo ignoring conflicts
+        save();
         return ResponseEntity.status(HttpStatus.OK)
                 .body(publisher);
     }
@@ -31,6 +61,7 @@ public class PublisherController implements PublishersApi {
         for (int i = 0; i< Publishers.size(); i++ ) {
             if(Publishers.get(i).getId() == id){
                 Publishers.remove(i);
+                save();
                 return ResponseEntity.status(HttpStatus.OK).body(null);
             }
         }
@@ -62,6 +93,7 @@ public class PublisherController implements PublishersApi {
                 possiblePublisher = publisher;
                 possiblePublisher.setId(id);
                 //save
+                save();
                 return ResponseEntity.status(HttpStatus.OK)
                         .body(possiblePublisher);
             }
