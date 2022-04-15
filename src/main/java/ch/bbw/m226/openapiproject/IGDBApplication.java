3675a034dc3a6962b959ea39b8b0a5d5ece60fd5
Remo Waelchli <remo.waelchli@icloud.com>
2022-04-15 14:53:42 +0200

    Testdaten hinzugefügt

--- src/main/java/ch/bbw/m226/openapiproject/IGDBApplication.java
+++ src/main/java/ch/bbw/m226/openapiproject/IGDBApplication.java
@@ -7,10 +7,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PonyApplication {
+public class IGDBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PonyApplication.class, args);
+		SpringApplication.run(IGDBApplication.class, args);
 	}
 
 	@Bean
